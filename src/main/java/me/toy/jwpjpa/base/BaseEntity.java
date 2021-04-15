b8561ddc692729a7351eb30f1d8ae0e4a9b71f94
slamdunk7575 <slamdunk7575@gmail.com>
2021-04-16 02:17:51 +0900

    (feat) BaseEntity 추가

---
+++ src/main/java/me/toy/jwpjpa/base/BaseEntity.java
@@ -0,0 +1,28 @@
+package me.toy.jwpjpa.base;
+
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(name = "created_time", updatable = false)
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    @Column(name = "modified_date")
+    private LocalDateTime modifiedDate;
+
+}
