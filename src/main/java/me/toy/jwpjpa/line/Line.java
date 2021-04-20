1f1eadf4952bb1774be2bcfbdb9dcf299ab22c3e
slamdunk7575 <slamdunk7575@gmail.com>
2021-04-20 19:54:49 +0900

    (feat) Line 연관관계 맵핑

--- src/main/java/me/toy/jwpjpa/line/Line.java
+++ src/main/java/me/toy/jwpjpa/line/Line.java
@@ -1,16 +1,23 @@
 package me.toy.jwpjpa.line;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.toy.jwpjpa.base.BaseEntity;
+import me.toy.jwpjpa.linestation.LineStation;
+import me.toy.jwpjpa.station.Station;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
+@Table(name = "line", indexes = {
+        @Index(name = "idx_line", columnList = "name", unique = true)
+})
 public class Line extends BaseEntity {
 
     @Column(name = "name", unique = true)
@@ -19,17 +26,33 @@ public class Line extends BaseEntity {
     @Column(name = "color")
     private String color;
 
-    private Line(String name, String color) {
+    @OneToMany(mappedBy = "line", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<LineStation> lineStations = new ArrayList<>();
+
+    @Builder
+    public Line(String name, String color, List<Station> stations) {
         this.name = name;
         this.color = color;
-    }
-
-    public static Line of(String name, String color) {
-        return new Line(name, color);
+        this.lineStations = Optional.ofNullable(stations).orElse(Collections.emptyList()).stream()
+        .map(station -> LineStation.of(this, station))
+        .collect(Collectors.toList());
     }
 
     public Line updateName(String name) {
         this.name = name;
         return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Line line = (Line) o;
+        return Objects.equals(name, line.name) && Objects.equals(color, line.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, color);
     }
 }
--- src/test/java/me/toy/jwpjpa/line/LineRepositoryTest.java
+++ src/test/java/me/toy/jwpjpa/line/LineRepositoryTest.java
@@ -1,5 +1,8 @@
 package me.toy.jwpjpa.line;
 
+import me.toy.jwpjpa.linestation.LineStation;
+import me.toy.jwpjpa.station.Station;
+import me.toy.jwpjpa.station.StationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.DirtiesContext;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,17 +26,44 @@ class LineRepositoryTest {
     @Autowired
     private LineRepository lineRepository;
 
+    @Autowired
+    private StationRepository stationRepository;
+
     @BeforeEach
     void setUp() {
-        lineRepository.save(Line.of("2호선", "초록색"));
-        lineRepository.save(Line.of("4호선", "파랑색"));
+        Station gangnam_station = stationRepository.save(Station.builder()
+                .name("강남역")
+                .build());
+
+        Station jamsil_station = stationRepository.save(Station.builder()
+                .name("잠실역")
+                .build());
+
+        Station pangyo_station = stationRepository.save(Station.builder()
+                .name("판교역")
+                .build());
+
+        lineRepository.save(Line.builder()
+                .name("2호선")
+                .color("초록색")
+                .stations(Arrays.asList(jamsil_station, gangnam_station))
+                .build());
+
+        lineRepository.save(Line.builder()
+                .name("신분당선")
+                .color("빨강색")
+                .stations(Arrays.asList(gangnam_station, pangyo_station))
+                .build());
     }
 
     @Test
     @DisplayName("Line 추가")
     void insert_line_test() {
         // given
-        Line line = Line.of("8호선", "분홍색");
+        Line line = Line.builder()
+                .name("8호선")
+                .color("분홍색")
+                .build();
 
         // when
         Line persistedLine = lineRepository.save(line);
@@ -65,7 +98,7 @@ class LineRepositoryTest {
         String changeName = "1호선";
 
         // when
-        Line line = lineRepository.findByName("4호선");
+        Line line = lineRepository.findByName("2호선");
         line.updateName(changeName);
 
         Line updatedLine = lineRepository.findByName(changeName);
@@ -73,7 +106,7 @@ class LineRepositoryTest {
         // then
         assertAll(
                 () -> assertThat(updatedLine.getName()).isEqualTo("1호선"),
-                () -> assertThat(updatedLine.getColor()).isEqualTo("파랑색")
+                () -> assertThat(updatedLine.getColor()).isEqualTo("초록색")
         );
     }
 
@@ -95,7 +128,10 @@ class LineRepositoryTest {
     @DisplayName("Line 전체 조회")
     void select_all_line_test() {
         // given
-        Line line = Line.of("8호선", "분홍색");
+        Line line = Line.builder()
+                .name("8호선")
+                .color("분홍색")
+                .build();
         lineRepository.save(line);
 
         // when
@@ -107,7 +143,33 @@ class LineRepositoryTest {
         // then
         assertAll(
                 () -> assertThat(lineNames.size()).isEqualTo(3),
-                () -> assertThat(lineNames).contains("8호선", "4호선", "2호선")
+                () -> assertThat(lineNames).contains("8호선", "신분당선", "2호선")
+        );
+    }
+
+    @Test
+    @DisplayName("노선 조회 시 속한 지하철역 조회")
+    void get_line_included_station_test() {
+        // given
+        Line line = lineRepository.findByName("2호선");
+        List<LineStation> lineStations = line.getLineStations();
+
+        // when
+        List<Station> stations = Optional.ofNullable(lineStations)
+                .orElse(Collections.emptyList()).stream()
+                .map(LineStation::getStation)
+                .collect(Collectors.toList());
+
+        // then
+        assertAll(
+                () -> assertThat(stations).hasSize(2),
+                () -> assertThat(stations).contains(
+                        Station.builder()
+                                .name("강남역")
+                                .build(),
+                        Station.builder()
+                                .name("잠실역")
+                                .build())
         );
     }
 
