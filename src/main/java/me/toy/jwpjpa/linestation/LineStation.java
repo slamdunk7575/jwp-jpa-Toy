bbbf704c4125cbc7a137d0ec6ff1ca9f9ae8e7c7
slamdunk7575 <slamdunk7575@gmail.com>
2021-04-23 00:43:41 +0900

    (refactor) LineStation 이전역 정보 추가

--- src/main/java/me/toy/jwpjpa/linestation/LineStation.java
+++ src/main/java/me/toy/jwpjpa/linestation/LineStation.java
@@ -1,9 +1,6 @@
 package me.toy.jwpjpa.linestation;
 
-import lombok.AccessLevel;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import me.toy.jwpjpa.base.BaseEntity;
 import me.toy.jwpjpa.line.Line;
 import me.toy.jwpjpa.station.Station;
@@ -25,12 +22,21 @@ public class LineStation extends BaseEntity {
     @JoinColumn(name = "station_id")
     private Station station;
 
-    private LineStation(Line line, Station station) {
+    @Embedded
+    private PreStationInfo preStationInfo;
+
+    @Builder
+    public LineStation(Line line, Station station, PreStationInfo preStationInfo) {
         this.line = line;
         this.station = station;
+        this.preStationInfo = preStationInfo;
     }
 
-    public static LineStation of(Line line, Station station) {
-        return new LineStation(line, station);
+    public Station preStation() {
+        return preStationInfo.getPreStation();
+    }
+
+    public int distance() {
+        return preStationInfo.getDistance();
     }
 }
